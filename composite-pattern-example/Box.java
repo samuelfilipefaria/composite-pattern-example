e52ac4377f042d6abb4037567c18786aba7e0e67
Samuel Filipe Faria <samuel.filipe@prevision.com.br>
2023-08-06 22:48:24 -0300

    feat(app): Implementing recursive logic

--- composite-pattern-example/Box.java
+++ composite-pattern-example/Box.java
@@ -2,30 +2,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Box implements Component {
-  private List<Product> products = new ArrayList<Product>();
+  private List content = new ArrayList();
 
-  public Box(List<Product> products) {
-    this.products = products;
+  public Box(List content) {
+    this.content = content;
   }
 
   @Override
   public Double calculatePrice() {
-    return 0.0;
+    Double amount = 0.0;
+
+    for(int i = 0; i < content.size(); i++) {
+      if (content.get(i) instanceof Product) {
+        Product product = (Product) content.get(i);
+        amount += product.calculatePrice();
+      } else {
+        Box box = (Box) content.get(i);
+        amount += box.calculatePrice();
+      }
+    }
+
+    return amount;
   }
 
   public void addProduct(Product p) {
-    this.products.add(p);
+    this.content.add(p);
   }
 
   public void removeProduct(Product p) {
-    this.products.remove(p);
+    this.content.remove(p);
   }
 
-  public List<Product> getProducts() {
-    return this.products;
+  public void addBox(Box b) {
+    this.content.add(b);
   }
 
-  public void setProducts(List<Product> products) {
-    this.products = products;
+  public void removeBox(Box b) {
+    this.content.remove(b);
+  }
+
+  public List getContent() {
+    return this.content;
+  }
+
+  public void setContent(List content) {
+    this.content = content;
   }
 }
